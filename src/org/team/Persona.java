df9d40d7b2dbe479f0382e4b567c32b48698face
Matias De Stefano <matiasdestefano97@gmail.com>
2018-10-11 21:51:08 -0300

    Clase Persona Realizada
    Agregado Constructor en clase Persona y getPregunta()

--- src/org/team/Persona.java
+++ src/org/team/Persona.java
@@ -1,5 +1,14 @@
 package org.team;
 
 public class Persona {
+	private char[] preguntas;
+	
+	public Persona(char[] preguntas) {
+		this.preguntas = preguntas;
+	}
 
+	public char[] getPreguntas() {
+		return preguntas;
+	}
+	
 }
