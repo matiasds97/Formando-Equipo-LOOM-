5c9fc261f3d29443e58fa645b1431d2e530ec914
Julian Peralta <julian.peralta.jp@gmail.com>
2018-10-11 21:52:07 -0300

    Se completo la clase equipo

--- src/org/team/Equipo.java
+++ src/org/team/Equipo.java
@@ -1,5 +1,29 @@
 package org.team;
 
 public class Equipo {
+	private int afinidad;
+	private Persona[] personas;
+	
+	public Equipo(Persona[] eq, int afinidad) {
+		this.personas = eq;
+		this.afinidad = afinidad;
+	}
 
+	public int getAfinidad() {
+		return afinidad;
+	}
+
+	public void setAfinidad(int afinidad) {
+		this.afinidad = afinidad;
+	}
+
+	public Persona[] getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(Persona[] personas) {
+		this.personas = personas;
+	}
+	
+	
 }
