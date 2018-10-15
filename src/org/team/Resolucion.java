8f55721d475bfd508c91639ec551877e21c05442
Matias De Stefano <matiasdestefano97@gmail.com>
2018-10-14 21:05:31 -0300

    LLegue a una solucion pero no anda jaj, depsues de este commit se rehace.

--- src/org/team/Resolucion.java
+++ src/org/team/Resolucion.java
@@ -14,17 +14,31 @@ public class Resolucion {
 	public void crearEquipos() {
 		int i, j, k;
 		int cantPreguntas = persona[0].getSumaValores().length;
+		int cantRespuestasIguales = 0;
+		boolean agregadoAlTeam = false;
+		boolean continuarBuscando = true;
+		ArrayList<Persona> aRemover = new ArrayList<Persona>();
 		Equipo equipo = new Equipo();
 		equipos = new ArrayList<Equipo>();
 		for(i = 0; i < cantPreguntas - 1; i++) {
 			for (Persona p : persona) //Agrega a todos los colaboradores al equipo.
 				equipo.agregarColaborador(p);
-			for(j = 0; j < cantPreguntas; j++) {
+			for(j = 0; j < cantPreguntas & continuarBuscando == true; j++) {
+				cantRespuestasIguales = j;
 				for(k = i + 1; k < persona.length; k++) {
 					if(persona[i].getSumaValores()[j] != persona[k].getSumaValores()[j])
-						equipo.removerColaborador(persona[k]);
+						aRemover.add(persona[k]);
+					else
+						agregadoAlTeam = true;
 				}
+				if(agregadoAlTeam == true) {
+					for (Persona p : aRemover)
+						equipo.removerColaborador(p);
+				}
+				else
+					continuarBuscando = false;
 			}
+			equipo.setRespuestasIguales(cantRespuestasIguales);
 			equipos.add(equipo);
 			equipo = new Equipo();
 		}
